868a2b9345ffd5ea647fedd9d5e1e6db8de5070f
Thomas Beacock <thomasbeacock@gmail.com>
2024-08-08 18:40:03 +0100

    Add content length and content type parsers

--- include/web/http/http_header_parser.h
+++ include/web/http/http_header_parser.h
@@ -1,22 +1,45 @@
 #pragma once
 
+#include "web/http/http_parser.h"
 #include "web/media/media_type.h"
 
-#include <parser/parser.h>
-
+#include <map>
 #include <string>
 #include <vector>
 
 namespace Web::Http {
-using AcceptHeader = std::vector<std::pair<Media::Type, Float>>;
+struct AcceptHeader {
+    struct Type {
+        Media::Type type;
+        Float quality;
+    };
+    std::vector<Type> types;
+};
 
-class HeaderParser : public Parsing::Parser {
+struct ContentLengthHeader {
+    Nat length;
+};
+
+struct ContentTypeHeader {
+    Media::Type type;
+    std::string charset;
+    std::string boundary;
+};
+
+class HeaderParser : public Parser {
   public:
     HeaderParser(std::string_view data);
 
     bool parse_accept(AcceptHeader &out_accept);
+    bool parse_content_length(ContentLengthHeader &out_content_length);
+    bool parse_content_type(ContentTypeHeader &out_content_type);
 
   protected:
     bool get_media_type(Media::Type &out_type);
+    bool get_quoted_char(Char &out_char);
+    bool get_quoted_pair(Char &out_char);
+    bool get_quoted_string(std::string &out_str);
+    bool get_parameter(std::string &out_key, std::string &out_value);
+    bool get_parameters(std::map<std::string, std::string> &out_parameters);
 };
 }  // namespace Web::Http
--- src/http/http_header_parser.cpp
+++ src/http/http_header_parser.cpp
@@ -1,8 +1,11 @@
 #include "web/http/http_header_parser.h"
 
+#include "util/util_string.h"
+
+#include <sstream>
+
 namespace Web::Http {
-Http::HeaderParser::HeaderParser(std::string_view data) : Parsing::Parser(data)
-{}
+Http::HeaderParser::HeaderParser(std::string_view data) : Parser(data) {}
 
 bool Http::HeaderParser::parse_accept(AcceptHeader &out_accept)
 {
@@ -35,7 +38,7 @@ bool Http::HeaderParser::parse_accept(AcceptHeader &out_accept)
             }
             get_whitespace();
         }
-        out_accept.push_back({media_type, quality});
+        out_accept.types.push_back({media_type, quality});
         if (!require(',')) {
             break;
         }
@@ -44,45 +47,145 @@ bool Http::HeaderParser::parse_accept(AcceptHeader &out_accept)
     return true;
 }
 
+bool Http::HeaderParser::parse_content_length(
+    ContentLengthHeader &out_content_length)
+{
+    return get_nat(out_content_length.length);
+}
+
+bool Http::HeaderParser::parse_content_type(ContentTypeHeader &out_content_type)
+{
+    if (!get_media_type(out_content_type.type)) {
+        return false;
+    }
+    std::map<std::string, std::string> params;
+    if (get_parameters(params)) {
+        if (auto it = params.find("charset"); it != params.end()) {
+            out_content_type.charset = it->second;
+        }
+        if (auto it = params.find("boundary"); it != params.end()) {
+            out_content_type.boundary = it->second;
+        }
+    }
+    return true;
+}
+
 bool Http::HeaderParser::get_media_type(Media::Type &out_type)
 {
     push_save();
-    if (is_eof() ||
-        !(in_bounds('0', '9') || in_bounds('a', 'z') || in_bounds('A', 'Z'))) {
-        pop_save();
+    if (!get_token(out_type.type)) {
+        load_save();
         return false;
     }
-    move_next();
-    while (!is_eof() &&
-           (is_equal('!') || in_bounds('#', '$') || is_equal('&') ||
-            is_equal('+') || in_bounds('-', '.') || in_bounds('0', '9') ||
-            in_bounds('A', 'Z') || in_bounds('^', '_') ||
-            in_bounds('a', 'z'))) {
-        move_next();
-    }
-    out_type.type = std::string(get_save_string());
-
     if (!require('/')) {
         load_save();
         return false;
     }
-
-    if (is_eof() ||
-        !(in_bounds('0', '9') || in_bounds('a', 'z') || in_bounds('A', 'Z'))) {
+    if (!get_token(out_type.subtype)) {
         load_save();
         return false;
     }
-    push_save();
-    move_next();
-    while (!is_eof() &&
-           (is_equal('!') || in_bounds('#', '$') || is_equal('&') ||
-            is_equal('+') || in_bounds('-', '.') || in_bounds('0', '9') ||
-            in_bounds('A', 'Z') || in_bounds('^', '_') ||
-            in_bounds('a', 'z'))) {
-        move_next();
-    }
-    out_type.subtype = std::string(get_save_string());
+    Util::make_lower(out_type.type);
+    Util::make_lower(out_type.subtype);
     pop_save();
-    return false;
+    return true;
+}
+
+bool Http::HeaderParser::get_quoted_char(Char &out_char)
+{
+    if (get_obs_char(out_char)) {
+        return true;
+    }
+
+    if (is_eof() || !(is_equal('\t') || is_equal(' ') || is_equal('!') ||
+                      in_bounds('#', '[') || in_bounds(']', '~'))) {
+        return false;
+    }
+    out_char = get_current();
+    move_next();
+    return true;
+}
+
+bool Http::HeaderParser::get_quoted_pair(Char &out_char)
+{
+    push_save();
+    if (!require('\\')) {
+        load_save();
+        return false;
+    }
+    if (get_obs_char(out_char) || get_visible_char(out_char)) {
+        pop_save();
+        return true;
+    }
+    if (is_eof() || !(is_equal('\t') || is_equal(' '))) {
+        load_save();
+        return false;
+    }
+    out_char = get_current();
+    pop_save();
+    return true;
+}
+
+bool Http::HeaderParser::get_quoted_string(std::string &out_str)
+{
+    push_save();
+    if (!require('\"')) {
+        load_save();
+        return false;
+    }
+    std::stringstream ss;
+    Char c;
+    while (get_quoted_char(c) || get_quoted_pair(c)) {
+        ss << c;
+    }
+    if (!require('\"')) {
+        load_save();
+        return false;
+    }
+    out_str = ss.str();
+    pop_save();
+    return true;
+}
+
+bool HeaderParser::get_parameter(std::string &out_key, std::string &out_value)
+{
+    push_save();
+    if (!get_token(out_key)) {
+        load_save();
+        return false;
+    }
+    if (!require('=')) {
+        load_save();
+        return false;
+    }
+    if (!(get_token(out_value) || get_quoted_string(out_value))) {
+        load_save();
+        return false;
+    }
+    Util::make_lower(out_key);
+    pop_save();
+    return true;
+}
+
+bool Http::HeaderParser::get_parameters(
+    std::map<std::string, std::string> &parameters)
+{
+    push_save();
+    std::string key, value;
+    while (true) {
+        push_save();
+        get_whitespace();
+        if (!require(';')) {
+            load_save();
+            break;
+        }
+        pop_save();
+        get_whitespace();
+        if (get_parameter(key, value)) {
+            parameters.insert({key, value});
+        }
+    }
+    pop_save();
+    return true;
 }
 }  // namespace Web::Http
